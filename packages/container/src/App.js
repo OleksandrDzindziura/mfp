499f799b0cc88cbe20e9ea0c632dcfe5ef4969f9
Oleksandr Dzindziura <o.dzyndzyura@lionwood.software>
2024-01-28 14:31:34 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MarketingApp from "./сomponents/MarketingApp";
+export default () => {
+    return <>
+
+        <h1>Hi there!</h1>
+        <MarketingApp />
+        </>
+}
